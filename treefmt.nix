000001009060a9963a11d7b8557e60b88b9f8fb2
MCSeekeri <mcseekeri@outlook.com>
2026-02-09 17:52:40 +0800

    style: 完善 treefmt-nix 的配置

--- treefmt.nix
+++ treefmt.nix
@@ -10,5 +10,40 @@
     statix.enable = true;
 
     rustfmt.enable = true;
+    dos2unix.enable = true;
+    keep-sorted.enable = true;
+    actionlint.enable = true;
+    autocorrect = {
+      enable = true;
+      settings = {
+        rules = {
+          space-word = "error";
+          space-punctuation = "error";
+          space-bracket = "error";
+          space-backticks = "error";
+          space-dash = "error";
+          space-dollar = "error";
+          fullwidth = "error";
+          no-space-fullwidth = "error";
+          halfwidth-word = "error";
+          halfwidth-punctuation = "error";
+          spellcheck = "error";
+        };
+        context.codeblock = "error";
+      };
+    };
+    sizelint = {
+      enable = true;
+      failOnWarn = true;
+    };
+    mdformat = {
+      enable = true;
+      plugins = ps: [ ps.mdformat-gfm ];
+      settings = {
+        wrap = "keep";
+        number = true;
+        end-of-line = "lf";
+      };
+    };
   };
 }
